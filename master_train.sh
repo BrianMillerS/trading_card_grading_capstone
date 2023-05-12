93eb4be82da3e5883eddcee9b96dc9a888e7790d
brismiller <brismiller@gmail.com>
2023-05-12 12:38:29 -0600

    added: arg to py script, py script for image flip augmentation

---
+++ master_train.sh
@@ -0,0 +1,5 @@
+# testing out a bunch of different params
+python RESNET-50.py --n_epochs 10 --base_path "/home/data/data_for_model_1" --droprate 0.012
+python RESNET-50.py --n_epochs 10 --base_path "/home/data/data_for_model_1" --droprate 0.025
+python RESNET-50.py --n_epochs 10 --base_path "/home/data/data_for_model_1" --droprate 0.05
+
