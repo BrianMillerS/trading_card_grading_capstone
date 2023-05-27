93047d068dd8e0a50ea4fc51565ecd97832eedee
brismiller <brismiller@gmail.com>
2023-05-26 19:03:52 -0600

    added documentation to eda script

--- data_processing/move_files_out_of_subfolders.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for folder in psa{1..10}; do
-    if [[ -d "$folder" ]]; then
-        echo "Moving files from $folder to parent directory"
-        mv "$folder"/* .
-    fi
-done
--- web_scraping/master_collectors_scrape.sh
+++ web_scraping/master_collectors_scrape.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-# python scrape_collections.py --grade 1 --card_num 3104 --files_dir /Users/brianmiller/Desktop/collection_files --photo_dir /Users/brianmiller/Desktop/collection_files/images_psa1 &
-# python scrape_collections.py --grade 2 --card_num 5352 --files_dir /Users/brianmiller/Desktop/collection_files --photo_dir /Users/brianmiller/Desktop/collection_files/images_psa2 &
-# python scrape_collections.py --grade 3 --card_num 7992 --files_dir /Users/brianmiller/Desktop/collection_files --photo_dir /Users/brianmiller/Desktop/collection_files/images_psa3 &
-# python scrape_collections.py --grade 4 --card_num 16288 --files_dir /Users/brianmiller/Desktop/collection_files --photo_dir /Users/brianmiller/Desktop/collection_files/images_psa4 &
-# python scrape_collections.py --grade 5 --card_num 18053 --files_dir /Users/brianmiller/Desktop/collection_files --photo_dir /Users/brianmiller/Desktop/collection_files/images_psa5 &
-# python scrape_collections.py --grade 6 --card_num 17917 --files_dir /Users/brianmiller/Desktop/collection_files --photo_dir /Users/brianmiller/Desktop/collection_files/images_psa6 &
-# python scrape_collections.py --grade 7 --card_num 40680 --files_dir /Users/brianmiller/Desktop/collection_files --photo_dir /Users/brianmiller/Desktop/collection_files/images_psa7 &
+python scrape_collections.py --grade 1 --card_num 3104 --files_dir /Users/brianmiller/Desktop/collection_files --photo_dir /Users/brianmiller/Desktop/collection_files/images_psa1 &
+python scrape_collections.py --grade 2 --card_num 5352 --files_dir /Users/brianmiller/Desktop/collection_files --photo_dir /Users/brianmiller/Desktop/collection_files/images_psa2 &
+python scrape_collections.py --grade 3 --card_num 7992 --files_dir /Users/brianmiller/Desktop/collection_files --photo_dir /Users/brianmiller/Desktop/collection_files/images_psa3 &
+python scrape_collections.py --grade 4 --card_num 16288 --files_dir /Users/brianmiller/Desktop/collection_files --photo_dir /Users/brianmiller/Desktop/collection_files/images_psa4 &
+python scrape_collections.py --grade 5 --card_num 18053 --files_dir /Users/brianmiller/Desktop/collection_files --photo_dir /Users/brianmiller/Desktop/collection_files/images_psa5 &
+python scrape_collections.py --grade 6 --card_num 17917 --files_dir /Users/brianmiller/Desktop/collection_files --photo_dir /Users/brianmiller/Desktop/collection_files/images_psa6 &
+python scrape_collections.py --grade 7 --card_num 40680 --files_dir /Users/brianmiller/Desktop/collection_files --photo_dir /Users/brianmiller/Desktop/collection_files/images_psa7 &
 python scrape_collections.py --grade 8 --card_num 47142 --files_dir /Users/brianmiller/Desktop/collection_files --photo_dir /Users/brianmiller/Desktop/collection_files/images_psa8 &
 python scrape_collections.py --grade 9 --card_num 94426 --files_dir /Users/brianmiller/Desktop/collection_files --photo_dir /Users/brianmiller/Desktop/collection_files/images_psa9 &
 python scrape_collections.py --grade 10 --card_num 101241 --files_dir /Users/brianmiller/Desktop/collection_files --photo_dir /Users/brianmiller/Desktop/collection_files/images_psa10 &
-
-# 8  91,996
-# 9  159,617
-# 10 189,656
